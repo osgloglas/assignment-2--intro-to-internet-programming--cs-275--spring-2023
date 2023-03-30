a4467940c37d69c3b5a6b3ea8def44aacc2c0cd8
Chris Johnson <jazztrombone007@gmail.com>
2023-03-30 11:37:33 -0400

    corrected gulp dependencies and added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+//dependencies
+const { src, dest, series } = require(`gulp`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const cssLinter = require(`gulp-stylelint`);
+const jsTranspiler = require(`gulp-babel`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
+
+//functions
+let compressHTML = () => {
+    return src(`*.html`)
+        .pipe(htmlCompressor({collapseWhitespace:true}))
+        .pipe(dest(`dev/html`));
+};
+
+let lintCSS = () => {
+    return src(`styles/*.css`)
+        .pipe(cssLinter())
+        .pipe(dest(`dev/css`));
+};
+
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter())
+        .pipe(dest(`dev/js`));
+};
+
+let fixJS = () => {
+    return src(`dev/*.js`)
+        .pipe(jsCompressor())
+        .pipe(jsTranspiler())
+        .pipe(dest(`dev/js`));
+};
+
+//export
+exports.default = series(
+    compressHTML,
+    lintCSS,
+    lintJS,
+    fixJS
+);
